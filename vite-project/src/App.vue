6779f3b0fdfbd819bee3f45f5d3fbd9be9495284
datnguyenpirago <117071633+datnguyenpirago@users.noreply.github.com>
2023-02-02 13:21:54 +0700

    delete service

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import { mapState} from 'vuex';
+  export default {
+    computed: {
+      ...mapState(['user'])
+    }
+  }
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+      {{ user }}
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ vite-project/src/store/index.js
@@ -0,0 +1,23 @@
+import { createStore } from 'vuex';
+const store = createStore({
+    state : {
+        user : {
+            data: {
+                name: 'zuza',
+            },
+            token: null,
+        }
+    },
+    getters: {
+
+    },
+    actions: {
+
+    }, mutations: {
+
+    }, modules : {
+        
+    }
+});
+
+export default store;
