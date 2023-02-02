6130dd5db2d0ae7bf1d3598cbdd96a2a26d906ce
datnguyenpirago <hunghkm01@gmail.com>
2023-02-02 23:02:51 +0700

    finish front end login

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,28 +1,19 @@
-<script>
-  import { mapState } from 'vuex';
-  export default {
-    computed: {
-      ...mapState(['user', 'admin'])
-    }
-  }
-</script>
+<script></script>
 
 <template>
-  <div>
-      {{ user }} {{ admin }}
-  </div>
+    <router-view></router-view>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+    .logo {
+        height: 6em;
+        padding: 1.5em;
+        will-change: filter;
+    }
+    .logo:hover {
+        filter: drop-shadow(0 0 2em #646cffaa);
+    }
+    .logo.vue:hover {
+        filter: drop-shadow(0 0 2em #42b883aa);
+    }
 </style>
--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import store  from './store'
-createApp(App).use(store).mount('#app')
+import { createApp } from 'vue';
+import './index.css';
+import App from './App.vue';
+import store from './store';
+import router from './router';
+createApp(App).use(store).use(router).mount('#app');
